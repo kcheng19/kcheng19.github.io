301ed528ec007978ece10a957ad9889a5886ad5f
Kevin Cheng <kcheng19@roslynschools.org>
2016-10-27 21:34:51 +0000

    Create Dodge game

--- Shooter/game.js
+++ Shooter/game.js
@@ -2,66 +2,141 @@
     var playerImage;
     var isGameOver;
     var enemy;
+    var enemy2;
     var enemyImage;
     var bullet;
     var isScore;
+    var started=false;
+    var bullet2;
+    var bullet3;
+    var enemy3;
 function preload(){
-    playerImage = loadImage("https://surrogate.hackedu.us/i.imgur.com/N5uCbDu.png");
-  enemyImage = loadImage("https://surrogate.hackedu.us/i.imgur.com/OdL0XPt.png");
-   backgroundImage = loadImage("https://surrogate.hackedu.us/i.imgur.com/aKQOg3G.png");
+    playerImage = loadImage("fly.gif");
+    enemyImage = loadImage("asteroid 2.png");
+    backgroundImage = loadImage("notebook-paper.jpg");
+    enemy2Image = loadImage("asteroidA.gif");
+    enemy3Image = loadImage("asteroidC.gif");
 }
 function setup() {
-    createCanvas(256,256);
-    player = createSprite(width/2, height-25, 0, 0);
+    createCanvas(1000,800);
+    player = createSprite(width/2, height-125, 0, 0);
     player.addImage(playerImage);
     enemy = createSprite(width/2, 0, 0, 0);
     enemy.addImage(enemyImage);
+    enemy.rotationSpeed = 3.0;
     isGameOver = false;
     bullet = createSprite(width/2, 0, 10, 30);
+    enemy2 = createSprite(2*width/7, 0, 0, 0);
+    enemy2.addImage(enemy2Image);
+    enemy2.rotationSpeed = 4.0;
+    bullet2 = createSprite(width/2, 0, 10, 30);
+    bullet3 = createSprite(width/2, 0, 10, 30);
     isScore = 0;
+    enemy3 = createSprite(6*width/7, 0, 0, 0);
+    enemy3. addImage(enemy3Image);
+    enemy3.rotationSpeed = 5.0;
 }
-
+$("#start").on("click",new_game);
 function draw() {
 
-     if (enemy.position.y > 256) {
+     if (enemy.position.y > 800) {
             gameOver();
-        }else{
+        }else if(started){
             if(enemy.overlap(player)){
                 isGameOver=true
             }
-                background(backgroundImage);
+    background(backgroundImage);
     drawSprites();
-     if (keyDown(RIGHT_ARROW) && player.position.x<(width-(playerImage.width/2))) {
+    if (keyDown(RIGHT_ARROW) && player.position.x<(width-(playerImage.width/2))) {
     player.position.x = player.position.x + 5;
-  }
+    }
     if (keyDown(LEFT_ARROW) && player.position.x>playerImage.width/2 ) {
     player.position.x = player.position.x - 5;
-  }
+    }
+  
     score();
-  enemy.position.y = enemy.position.y + 2
-  if (enemy.overlap(bullet)) {
-  enemy.position.y = 0;
-  enemy.position.x = random(5, width-5);
-  isScore = isScore + 1;
-}
-        bullet.position.y = bullet.position.y - 5
-        if(bullet.position.y < 0 ){
-            bullet.position.y = 256;
-            bullet.position.x = player.position.x;
+    enemy.position.y = enemy.position.y + 2
+    if (enemy.overlap(bullet)) {
+    enemy.position.y = 0;
+    enemy.position.x = random(100, width-100);
+    isScore = isScore + 1;
+    }
+    
+    if (isScore>20){
+        enemy.position.y = enemy.position.y + 2;
+        enemy2.position.y = enemy2.position.y + 2;
+        enemy3.position.y = enemy3.position.y + 2;
+        if (keyDown(RIGHT_ARROW) && player.position.x<(width-(playerImage.width/2))) {
+        player.position.x = player.position.x + 7;
+        }
+        if (keyDown(LEFT_ARROW) && player.position.x>playerImage.width/2 ) {
+        player.position.x = player.position.x - 7;
+        }
+        bullet.position.y = bullet.position.y - 7
+        bullet2.position.y = bullet2.position.y - 17
+        if(bullet2.position.y < 0 ){
+            bullet2.position.y = 600;
+            bullet2.position.x = player.position.x - 50;
+        }
+        bullet3.position.y = bullet3.position.y - 17
+        if(bullet3.position.y < 0 ){
+            bullet3.position.y = 600;
+            bullet3.position.x = player.position.x + 50;
         }
     }
+    if (enemy.overlap(bullet2)) {
+            enemy.position.y = 0;
+            enemy.position.x = random(100, width-100);
+            isScore = isScore + 1;
+            }
+    if (enemy.overlap(bullet3)) {
+            enemy.position.y = 0;
+            enemy.position.x = random(100, width-100);
+            isScore = isScore + 1;
+            }
 
-
+    bullet.position.y = bullet.position.y - 10
+    if(bullet.position.y < 0 ){
+        bullet.position.y = 600;
+        bullet.position.x = player.position.x;
+        }
+    }
+    if (enemy2.position.y > 800) {
+            gameOver();
+        }else{
+            if(enemy2.overlap(player)){
+                isGameOver=true
+            }
+        enemy2.position.y = enemy2.position.y + 2
+        if (enemy2.overlap(bullet)) {
+        enemy2.position.y = 0;
+        enemy2.position.x = random(100, width-100);
+        isScore = isScore + 1;
+        }
+    }
+    if (enemy3.position.y > 800) {
+            gameOver();
+        }else{
+            if(enemy3.overlap(player)){
+                isGameOver=true
+            }
+        enemy3.position.y = enemy3.position.y + 2
+        if (enemy3.overlap(bullet)) {
+        enemy3.position.y = 0;
+        enemy3.position.x = random(100, width-100);
+        isScore = isScore + 1;
+        }
+    }
 }
 function gameOver() {
-  background(0);
-  textAlign(CENTER);
-  textFont("Georgia");
-  fill("white");
-  text("Game Over!", width/2, height/2);
-  text("Your score was", width/2, 5*height/8);
-  text(isScore, width/2, 6*height/8);
-  text("Click anywhere to try again", width/2, 7*height/8);
+    background(0);
+    textAlign(CENTER);
+    textFont("Georgia");
+    fill("white");
+    text("Game Over!", width/2, height/2);
+    text("Your score was", width/2, 5*height/8);
+    text(isScore, width/2, 6*height/8);
+    text("Click anywhere to try again", width/2, 7*height/8);
 }
 
  
@@ -75,6 +150,12 @@ function mouseClicked(){
 
 function score(){
     textFont("Arial");
-    fill("white");
-     text(isScore,  70*width/100, 10*height/100);
+    fill("black");
+    text(isScore,  800,100);
+}
+function new_game(){
+    started=true;
+}
+function touchStarted(){
+    mouseClicked()
 }
